719e201fbc9a7e378165a5376e06b519b6c89cfc
Shrestha210 <maheshwarishrestha90@gmail.com>
2025-07-03 02:00:20 +0530

    Create capicator.config.ts

---
+++ src/capicator.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.saksham.teenpatti',
+  appName: 'Teen Patti Saksham',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
